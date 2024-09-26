10e5009d3f8b1271067ec3786a360eacb1e6eba1
ganesh-d2001 <bhoomikas@devtools.in>
2024-09-26 17:06:18 +0530

    Test5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,9 @@ service postgresql start
 sleep 5
 
 # Set up the database and user if they don't already exist
-su - postgres -c "psql -tc \"SELECT 1 FROM pg_database WHERE datname = '$prod'\"" | grep -q 1 || su - postgres -c "psql --command \"CREATE DATABASE $prod;\""
+su - postgres -c "psql -tc \"SELECT 1 FROM pg_database WHERE datname = 'prod'\"" | grep -q 1 || su - postgres -c "psql --command \"CREATE DATABASE prod;\""
 su - postgres -c "psql -tc \"SELECT 1 FROM pg_roles WHERE rolname = 'prod'\"" | grep -q 1 || su - postgres -c "psql --command \"CREATE USER prod WITH SUPERUSER PASSWORD 'prod';\""
-su - postgres -c "psql --command \"GRANT ALL PRIVILEGES ON DATABASE $prod TO prod;\""
+su - postgres -c "psql --command \"GRANT ALL PRIVILEGES ON DATABASE prod TO prod;\""
 
 # Run the Python application
 exec "$@"
