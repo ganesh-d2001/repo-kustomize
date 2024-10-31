1e5802f8e710894ca5f852fdb3a7729aac8672d6
ganesh-d2001 <bhoomikas@devtools.in>
2024-10-31 17:36:24 +0530

    d

--- Backend.py
+++ Backend.py
@@ -71,17 +71,17 @@ def serve_index():
     return render_template('index.html')
 
 # Serve the people submission form page at /app/Frontend.html
-@app.route('/Frontend.html')
+@app.route('/app/Frontend.html')
 def serve_submission():
     return render_template('Frontend.html')
 
 # Serve the people data page at /app/people_data.html
-@app.route('/people_data.html')
+@app.route('/app/people_data.html')
 def serve_people_data():
     return render_template('people_data.html')
 
 # Serve the person detail page at /app/person_detail.html
-@app.route('/person_detail.html')
+@app.route('/app/person_detail.html')
 def serve_person_detail():
     return render_template('person_detail.html')
 
