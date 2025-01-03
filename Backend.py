0e3936e7bf55c6af65981540817e01653d9c88b4
ganesh-d2001 <bhoomikas@devtools.in>
2025-01-03 15:18:34 +0530

    sc

--- Backend.py
+++ Backend.py
@@ -106,7 +106,7 @@ def submit_data():
         return jsonify({"status": "error", "message": str(e)}), 400
 
 # Endpoint to fetch people data at /app/people
-@app.route('/people', methods=['GET'])
+@app.route('/app/people', methods=['GET'])
 def fetch_people_data():
     try:
         with connect_to_db() as connection:
@@ -124,7 +124,7 @@ def fetch_people_data():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 # Fetch a person's detail at /app/people/<int:person_id>
-@app.route('/people/<int:person_id>', methods=['GET'])
+@app.route('/app/people/<int:person_id>', methods=['GET'])
 def fetch_person_detail(person_id):
     try:
         with connect_to_db() as connection:
@@ -147,7 +147,7 @@ def fetch_person_detail(person_id):
         return jsonify({"status": "error", "message": str(e)}), 500
 
 # Search for a person by name at /app/people/search
-@app.route('/people/search', methods=['GET'])
+@app.route('/app/people/search', methods=['GET'])
 def search_people_by_name():
     name = request.args.get('name')
     try:
