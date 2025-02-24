17989e92c22d44489165cc39e3b102b065aed32a
ganesh-d2001 <bhoomikas@devtools.in>
2025-02-24 18:06:29 +0530

    changes in prot in postgres ser and Backend for db

--- Backend.py
+++ Backend.py
@@ -20,7 +20,7 @@ DB_HOST = os.getenv('DB_HOST', 'postgresql-db-service-test')
 DB_USER = os.getenv('DB_USER', 'postgres')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'Test@123')
 DB_NAME = os.getenv('DB_NAME', 'test')
-DB_PORT = os.getenv('DB_PORT', '5434')
+DB_PORT = os.getenv('DB_PORT', '5432')
 
 # Logging setup
 log_directory = 'logs'
